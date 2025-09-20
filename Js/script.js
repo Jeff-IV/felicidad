062ec31f30cdb35c6c1e0a8b0e2a78125569539d
Jeff-IV <jefegarcia2003@gmail.com>
2025-09-19 19:49:26 -0500

    Update script.js recovery

--- Js/script.js
+++ Js/script.js
@@ -145,11 +145,6 @@ function startFloatingObjects() {
 // --- Música de fondo ---
 function playBackgroundMusic() {
   const audio = document.getElementById('bg-music');
-  if (audio.muted) {
-    audio.muted = false;
-    audio.volume = 0.3;
-    audio.play();
-  }
   
   if (!audio) return;
 
